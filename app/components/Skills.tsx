cce16eb522c6c1e9cfed5e9968c41867ff371d1b
zeynab.mvs <zeynab.mvs@gmail.com>
2025-07-03 10:59:06 +0330

    micro interaction: for skill item hover

--- app/components/Skills.tsx
+++ app/components/Skills.tsx
@@ -20,8 +20,8 @@ const SkillItemWithIcon: React.FC<{ skill: Skill }> = ({ skill }) => (
 );
 
 const SkillItem: React.FC<{ skill: Skill }> = ({ skill }) => (
-  <div className="inline-flex items-center px-3 py-1 bg-zinc-100 dark:bg-zinc-700 rounded-full hover:bg-zinc-700 dark:hover:bg-zinc-200 group cursor-default transition-all duration-150">
-    <span className="text-primary dark:text-primary-dark text-xs group-hover:text-zinc-100 group-hover:dark:text-zinc-800">{skill.name}</span>
+  <div className="inline-flex items-center px-3.5 py-1.5 bg-zinc-100 dark:bg-zinc-700 rounded-full cursor-default transition-all duration-200 opacity-85 scale-90 hover:scale-100 hover:opacity-100">
+    <span className="text-primary dark:text-primary-dark text-xs">{skill.name}</span>
   </div>
 );
 
@@ -31,7 +31,7 @@ const SkillsList: React.FC<{ skills: SkillsGroupType }> = ({ skills }) => {
   return (
     <div className="card p-4 md:p-6 md:flex-1/3">
       <h2 className="text-2xl font-bold mb-6">Skills</h2>
-      <div className="flex flex-wrap gap-3 lg:gap-4 ">
+      <div className="flex flex-wrap gap-2.5 lg:gap-3 ">
         {allSkills.map((skill) => (
           <SkillItem key={skill.name} skill={skill} />
         ))}
